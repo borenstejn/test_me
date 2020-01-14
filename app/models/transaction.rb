2318e7afa391ecc8e0c9b3febe4edf9a7ba96f99
ccocchi <cocchi.c@gmail.com>
2020-01-14 12:09:31 +0100

    Add validations on the model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
-  validates :owner, presence: true
+  validates :owner,   presence: true
+  validates :amount,  presence: true, numericality: { only_integer: true }
 end
